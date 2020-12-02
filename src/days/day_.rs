f08fa0577d9074bdedf0256b1df997d14c29221c
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2020-12-02 19:39:36 +0000

    Add a day template

---
+++ src/days/day_.rs
@@ -0,0 +1,14 @@
+extern crate advent;
+use self::advent::*;
+
+use std::collections::HashMap;
+
+pub fn run() {
+    let filename = "inputs/day__.txt";
+    let inputs = read_inputs(&filename);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
