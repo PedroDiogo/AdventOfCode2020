0c187a3b3c35dca3e44f58c501c7b0103e71790f
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2020-12-02 19:15:18 +0000

    Day 2, part 2

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -9,14 +9,17 @@ pub fn run() {
     let inputs = read_inputs(&filename);
     let inputs = inputs.split("\n").collect();
 
-    let part_one = number_of_valid_passwords(&inputs);
+    let part_one = number_of_valid_passwords(&inputs, valid_password);
     println!("Part one: {}", part_one);
+
+    let part_two = number_of_valid_passwords(&inputs, valid_password_part_two);
+    println!("Part two: {}", part_two);
 }
 
-fn number_of_valid_passwords(passwords: &Vec<&str>) -> usize {
+fn number_of_valid_passwords(passwords: &Vec<&str>, validator_function: fn(&usize, &usize, &char, &str) -> bool) -> usize {
     passwords.iter()
     .map(|password| scan_fmt!(password, "{d}-{d} {[a-z]}: {}", usize, usize, char, String).unwrap())
-    .filter(|(min, max, character, password)| valid_password(min, max, character, password))
+    .filter(|(min, max, character, password)| validator_function(min, max, character, password))
     .count()
 }
 
@@ -30,6 +33,14 @@ fn valid_password(min: &usize, max: &usize, character: &char, password: &str) ->
     } else {
         false
     }
+}
+
+fn valid_password_part_two(position_1: &usize, position_2: &usize, character:&char, password: &str) -> bool {
+    let char_position1 = password.chars().nth(*position_1-1).expect("Expected a character in position 1");
+    let char_position2 = password.chars().nth(*position_2-1).expect("Expected a character in position 2");
+    let valid_password = char_position1 != char_position2 && (char_position1 == *character || char_position2 == *character);
+
+    valid_password
 }
 
 fn character_distribution(line: &str) -> HashMap<char, usize> {
@@ -51,12 +62,19 @@ mod tests {
     #[test]
     fn test_number_of_valid_passwords() {
         let passwords = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
-        assert_eq!(2, number_of_valid_passwords(&passwords));
+        assert_eq!(2, number_of_valid_passwords(&passwords, valid_password));
     }
     #[test]
     fn test_valid_password() {
         assert_eq!(true, valid_password(&1usize, &3usize, &'a', "abcde"));
         assert_eq!(false, valid_password(&2usize, &3usize, &'a', "abcde"));
+    }
+
+    #[test]
+    fn test_valid_password_part_two() {
+        assert_eq!(true, valid_password_part_two(&1usize, &3usize, &'a', "abcde"));
+        assert_eq!(false, valid_password_part_two(&1usize, &3usize, &'b', "cdefg"));
+        assert_eq!(false , valid_password_part_two(&1usize, &3usize, &'c', "ccccccccc"));
     }
 
     #[test]
