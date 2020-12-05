2d7f10f42cdad2e1aded0e2c2ed2b4414fd6f096
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2020-12-05 13:24:18 +0000

    Tidy up day template

--- src/days/day_.rs
+++ src/days/day_.rs
@@ -4,6 +4,12 @@ use self::advent::*;
 pub fn run() {
     let filename = "inputs/day__.txt";
     let inputs = read_inputs(&filename);
+
+    let part_one = 0;
+    println!("Part one: {}", part_one);
+
+    let part_two = 0;
+    println!("Part two: {}", part_two);
 }
 
 #[cfg(test)]
