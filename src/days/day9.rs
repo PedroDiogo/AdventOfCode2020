1b132cf13911c1a924317782899f0f702ea1f70a
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2020-12-20 12:20:20 +0000

    Day 9, part 2

--- src/days/day9.rs
+++ src/days/day9.rs
@@ -1,6 +1,10 @@
 use super::lib::*;
 
+use std::cmp::Ordering;
 use std::collections::HashSet;
+
+extern crate itertools;
+use self::itertools::Itertools;
 
 pub fn run() -> (Option<String>, Option<String>) {
     let filename = "inputs/day9.txt";
@@ -11,8 +15,11 @@ pub fn run() -> (Option<String>, Option<String>) {
         .filter_map(|x| *x)
         .collect();
 
-    let part_one = Some(find_first_number_not_sum_of_two_previous(&inputs, 25).to_string());
-    let part_two = None;
+    let part_one_solution = find_first_number_not_sum_of_two_previous(&inputs, 25);
+    let part_one = Some(part_one_solution.to_string());
+    let part_two_solution = find_continuous_list_that_sums_to_number(&inputs, &part_one_solution);
+    let part_two_solution = part_two_solution.iter().minmax().into_option().unwrap();
+    let part_two = Some((part_two_solution.0 + part_two_solution.1).to_string());
 
     (part_one, part_two)
 }
@@ -46,17 +53,45 @@ fn is_number_sum_of_two_others(number: &usize, previous_numbers: &HashSet<usize>
         .any(|x| x)
 }
 
+fn find_continuous_list_that_sums_to_number(inputs: &[usize], number: &usize) -> Vec<usize> {
+    let mut tail_idx = 0;
+    let mut head_idx = 1;
+    let mut sum: usize = inputs.get(tail_idx..=head_idx).unwrap().iter().sum();
+
+    loop {
+        match sum.cmp(number) {
+            Ordering::Greater => {
+                sum -= inputs.get(tail_idx).unwrap();
+                tail_idx += 1;
+            }
+            Ordering::Less => {
+                head_idx += 1;
+                sum += inputs.get(head_idx).unwrap();
+            }
+            Ordering::Equal => break,
+        }
+    }
+    inputs.get(tail_idx..=head_idx).unwrap().to_vec()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const NUMBERS: [usize; 20] = [
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,
+    ];
+
     #[test]
     fn test_find_first_number_not_sum_of_two_previous() {
-        let numbers = [
-            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
-            576,
-        ];
+        assert_eq!(127, find_first_number_not_sum_of_two_previous(&NUMBERS, 5));
+    }
 
-        assert_eq!(127, find_first_number_not_sum_of_two_previous(&numbers, 5));
+    #[test]
+    fn test_find_continuous_list_that_sums_to_number() {
+        assert_eq!(
+            [15, 25, 47, 40].to_vec(),
+            find_continuous_list_that_sums_to_number(&NUMBERS, &127),
+        );
     }
 }
