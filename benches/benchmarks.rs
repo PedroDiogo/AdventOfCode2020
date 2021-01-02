91a1608b9d573f4ba9d53c9244dd1f50f5ded1a8
Pedro Diogo <158898+PedroDiogo@users.noreply.github.com>
2021-01-02 12:24:47 +0000

    Updated benches

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -18,6 +18,22 @@ benchmark!(day3, day3::run);
 benchmark!(day4, day4::run);
 benchmark!(day5, day5::run);
 benchmark!(day6, day6::run);
+benchmark!(day7, day7::run);
+benchmark!(day8, day8::run);
+benchmark!(day9, day9::run);
+benchmark!(day10, day10::run);
+benchmark!(day11, day11::run);
+benchmark!(day12, day12::run);
+benchmark!(day13, day13::run);
+benchmark!(day14, day14::run);
+benchmark!(day15, day15::run);
+benchmark!(day16, day16::run);
+benchmark!(day17, day17::run);
+benchmark!(day18, day18::run);
+benchmark!(day19, day19::run);
 
-criterion_group!(benches, day1, day2, day3, day4, day5, day6);
+criterion_group!(
+    benches, day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13,
+    day14, day15, day16, day17, day18, day19
+);
 criterion_main!(benches);
